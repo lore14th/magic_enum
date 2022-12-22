d1833c85ec1afcfa59eafeaf284e1149cf72ad7e
lore14th <lorenzo.fortina.95@gmail.com>
2022-12-22 10:28:38 +0100

    Added premake script

---
+++ premake5.lua
@@ -0,0 +1,37 @@
+project "magic_enum"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "Off"
+	
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"magic_enum.hpp",
+		"magic_enum_format.hpp",
+		"magic_enum_fuse.hpp",
+		"magic_enum_switch.hpp"
+	}
+
+	defines 
+	{
+	}
+	
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "On"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "On"
+		symbols "On"
+
+	filter "configurations:Distribution"
+		runtime "Release"
+		optimize "On"
+		symbols "Off"
